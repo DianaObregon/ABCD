a7510c4692fd49233e0dfb47fb3bf1adb0e02aed
DianaObregon <dianaestefaniaobregoncalderon@gmail.com>
2015-03-14 12:15:50 -0500

    se agrego una clase nueva

--- src/ABCD.java
+++ src/ABCD.java
@@ -1,5 +1,5 @@
 
-package abcd;
+
 // autor: Diana Estefania Obregon
 import javax.swing.JOptionPane;
 public class ABCD {
@@ -7,5 +7,6 @@ public class ABCD {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog (null," Hola bienvenidos a Github");
     }
+    
     
 }
---
+++ src/NewClass.java
@@ -0,0 +1,9 @@
+
+
+/**
+ *
+ * @author Diana Obregon
+ */
+public class NewClass {
+    
+}
